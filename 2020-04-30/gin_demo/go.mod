e4137d2a94a6e97b459ab9e2a1015927f96faf65
hank <kslhy456@163.com>
2020-07-22 17:51:55 +0800

    gin add session的使用

--- 2020-04-30/gin_demo/go.mod
+++ 2020-04-30/gin_demo/go.mod
@@ -2,4 +2,7 @@ module gin_demo
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/icza/session v1.2.0
+)
--- 2020-04-30/gin_demo/main.go
+++ 2020-04-30/gin_demo/main.go
@@ -1,21 +1,88 @@
 package main
-import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-)
-func main(){
-	r := gin.Default()
-	r.GET("/index", func(c *gin.Context) {
-		c.JSON(http.StatusOK,gin.H{
-			"name":1,
-		})
 
+import (
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/gin-gonic/gin"
+	"github.com/icza/session"
+)
+
+func middle(c *gin.Context) {
+	m := map[string]interface{}{}
+	r := c.Request
+	w := c.Writer
+	var templ = template.Must(template.New("").Parse(page))
+	sess := session.Get(r)
+	if sess != nil {
+		// Already logged in
+		if r.FormValue("Logout") != "" {
+			session.Remove(sess, w) // Logout user
+			sess = nil
+		} else {
+			sess.SetAttr("Count", sess.Attr("Count").(int)+1)
+		}
+	} else {
+		// Not logged in
+		if r.FormValue("Login") != "" {
+			if userName := r.FormValue("UserName"); userName != "" && r.FormValue("Password") == "aDeexsdfeq234" {
+				// Successful login. New session with initial constant and variable attributes:
+				sess = session.NewSessionOptions(&session.SessOptions{
+					CAttrs: map[string]interface{}{"UserName": userName},
+					Attrs:  map[string]interface{}{"Count": 1},
+				})
+				session.Add(sess, w)
+			} else {
+				m["InvalidLogin"] = true
+			}
+		}
+	}
+
+	if sess != nil {
+		m["UserName"] = sess.CAttr("UserName")
+		m["Count"] = sess.Attr("Count")
+	}
+	if err := templ.Execute(w, m); err != nil {
+		log.Println("Error:", err)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"name": 1,
 	})
+}
+
+func main() {
+	session.Global.Close()
+	session.Global = session.NewCookieManagerOptions(session.NewInMemStore(), &session.CookieMngrOptions{AllowHTTP: true})
+	r := gin.Default()
+	r.GET("/index", middle)
+	r.POST("/index", middle)
 	r.POST("login", func(c *gin.Context) {
-		
+
 		c.JSON(http.StatusOK, gin.H{
-			"he":1,
+			"he": 1,
 		})
 	})
 	r.Run(":9000")
 }
+
+const page = `<html><body>
+{{if .InvalidLogin}}<p style="color:red">Invalid user name or password!</p>{{end}}
+
+{{if .UserName}}
+	<p>Hello <b>{{.UserName}}</b>! Since login you visited <b>{{.Count}}</b> times! <a href="/index">Refresh!</a></p>
+{{end}}
+
+<form method="post" action="/index">
+	{{if .UserName}}
+		<input type="submit" name="Logout" value="Logout">
+	{{else}}
+		<label for="UserNameId" style="width:100px; display: inline-block">User name:</label>
+		<input type="text" name="UserName" id="UserNameId"><br>
+		<label for="PasswordId" style="width:100px; display: inline-block">Password:</label>
+		<input type="password" name="Password" id="PasswordId">
+		<span style="font-style:italic; font-size: 90%">Tip: use 'a' to login ;)</span><br>
+		<input type="submit" name="Login" value="Login">
+	{{end}}
+</form>
+</body></html>`
