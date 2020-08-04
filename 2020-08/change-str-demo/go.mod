61f28ba66bec7fffa0aef1e2485d377636a9ad13
hank <kslhy456@163.com>
2020-08-04 15:58:08 +0800

    拆，解，合，copy 字符串

---
+++ 2020-08/change-str-demo/go.mod
@@ -0,0 +1,3 @@
+module github.com/agocan/go-demo/2020-08/change-str-demo
+
+go 1.14
---
+++ 2020-08/change-str-demo/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	dockerfile := `FROM alpine
+RUN echo 123
+RUN echo 321
+`
+	strSlice := strings.Split(dockerfile, "\n")
+	for i, str := range strSlice {
+		if str == "" {
+			continue
+		}
+		if strings.HasPrefix(str, "FROM") {
+			strSlice[i] = fmt.Sprintf("FROM %s", "alpine:3.11")
+		}
+	}
+	midStr := strings.Join(strSlice, "\n")
+	fmt.Println(midStr)
+	new := strings.Replace(midStr, "123", "333", 1)
+	fmt.Println(new)
+}
