b073c6c257392432b39260b3e1238ad19d1e8555
hank <kslhy456@163.com>
2020-08-05 10:35:08 +0800

    add swagger docs

---
+++ 2020-08/gin-swagger-demo/README.md
@@ -0,0 +1,11 @@
+
+https://github.com/swaggo/gin-swagger
+
+![https://github.com/swaggo/swag/blob/master/README_zh-CN.md#%E5%BF%AB%E9%80%9F%E5%BC%80%E5%A7%8B](https://github.com/swaggo/swag/blob/master/README_zh-CN.md#%E5%BF%AB%E9%80%9F%E5%BC%80%E5%A7%8B)
+
+```
+go get -u github.com/swaggo/swag/cmd/swag
+swag init
+go get -u github.com/swaggo/gin-swagger
+go get -u github.com/swaggo/files
+```
---
+++ 2020-08/gin-swagger-demo/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "github.com/agocan/go-demo/2020-08/gin-swagger-demo/docs"
+)
+
+// @Summary 新增文章标签
+// @Produce  json
+// @Param name query string true "Name"
+// @Param state query int false "State"
+// @Param created_by query int false "CreatedBy"
+// @Success 200 {string} json "{"code":200,"data":{},"msg":"ok"}"
+// @Router /api/v1/tags [get]
+func handler(c *gin.Context) {
+
+}
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
+func main() {
+	r := gin.New()
+
+	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json") // The url pointing to API definition
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+	r.GET("//api/v1/tags", handler)
+	r.Run()
+}
